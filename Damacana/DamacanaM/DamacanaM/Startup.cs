a4bd05b2845fdd773e8acbb030528c8133966b91
Muratcan Kocabıyıkoğlu <muratcan.kocabiyikoglu@gmail.com>
2015-12-02 00:14:44 +0200

    Damacana and .gitignore created

---
+++ Damacana/DamacanaM/DamacanaM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DamacanaM.Startup))]
+namespace DamacanaM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
